5f6f9ebb2ed2554afb514337fb9c98c5963e7dbb
Balaje K <balaje6@gmail.com>
2023-02-08 11:49:09 +0100

    Changes to plotting the functions

--- SimplifiedHigherOrder/1dFunctions.jl
+++ SimplifiedHigherOrder/1dFunctions.jl
@@ -17,14 +17,11 @@ include("preallocate_matrices.jl")
 Problem data 1: Smooth coefficient
 =#
 # Problem data
-D₁(x) = @. 0.5
-f(x) = @. 1.0
-u(x) = @. x*(1-x)
-∇u(x) = ForwardDiff.derivative(u,x)
-domain = (0.0,1.0)
-plt3 = plot()
-plot_nds = domain[1]:2^-3:domain[2]
-plot!(plt3, plot_nds, D₁.(plot_nds), lc=:black, lw=2, label="Diffusion Coefficient")
+# D₁(x) = @. 0.5
+# f(x) = @. 1.0
+# u(x) = @. x*(1-x)
+# ∇u(x) = ForwardDiff.derivative(u,x)
+# domain = (0.0,1.0)
 
 #=
 Problem data 2: Oscillatory diffusion coefficient
@@ -32,37 +29,28 @@ Problem data 2: Oscillatory diffusion coefficient
 # domain = (0.0,1.0)
 # D₂(x) = (2 + cos(2π*x/(2^-6)))^-1
 # f(x) = 0.5*π^2*sin(π*x)
-# plt3 = plot()
-# plot_nds = domain[1]:2^-7:domain[2]
-# plot!(plt3, plot_nds, D₂.(plot_nds), lc=:black, lw=2, label="Diffusion Coefficient")
 
 #=
 Problem data 3: Random diffusion coefficient
 =#
-# domain = (0.0,1.0)
-# f(x) = sin(5π*x)
-# Nₑ = 2^12
-# nds_micro = LinRange(domain[1], domain[2], Nₑ+1)
-# diffusion_micro = 0.5 .+ 4.5*rand(Nₑ+1)
-# function _D(x::Float64, nds_micro::AbstractVector{Float64}, diffusion_micro::Vector{Float64})
-#   nₑ = size(nds_micro,1)
-#   for i=1:nₑ
-#     if(nds_micro[i] ≤ x ≤ nds_micro[i+1])      
-#       return diffusion_micro[i+1]
-#     else
-#       continue
-#     end 
-#   end
-# end
-# function D₃(x::Float64; nds_micro = nds_micro, diffusion_micro = diffusion_micro)
-#   _D(x, nds_micro, diffusion_micro)
-# end
-# plt3 = plot()
-# for i=1:Nₑ-1
-#   plot!(plt3, [nds_micro[i], nds_micro[i+1]], [D₃(nds_micro[i]), D₃(nds_micro[i])], lc=:black, lw=2, label="")
-# end
-# plot!(plt3, [nds_micro[Nₑ], nds_micro[Nₑ+1]], [D₃(nds_micro[Nₑ]), D₃(nds_micro[Nₑ])], lc=:black, lw=2, label="Diffusion coefficient")
-# ylims!(plt3,(0,10))
+domain = (0.0,1.0)
+Nₑ = 2^7
+nds_micro = LinRange(domain[1], domain[2], Nₑ+1)
+diffusion_micro = 0.5 .+ 4.5*rand(Nₑ+1)
+function _D(x::Float64, nds_micro::AbstractVector{Float64}, diffusion_micro::Vector{Float64})
+  nₑ = size(nds_micro,1)
+  for i=1:nₑ
+    if(nds_micro[i] ≤ x ≤ nds_micro[i+1])      
+      return diffusion_micro[i+1]
+    else
+      continue
+    end 
+  end
+end
+function D₃(x::Float64; nds_micro = nds_micro, diffusion_micro = diffusion_micro)
+  _D(x, nds_micro, diffusion_micro)
+end
+f(x) = sin(5π*x)
 
 #=
 FEM parameters
@@ -80,7 +68,7 @@ Solve the saddle point problems to obtain the new basis functions
 
 preallocated_data = preallocate_matrices(domain, nc, nf, l, (q,p));
 cache = basis_cache(q), zeros(Float64,p+1), quad, preallocated_data
-compute_ms_basis!(cache, nc, q, p, D₁) 
+compute_ms_basis!(cache, nc, q, p, D₃) 
 #=
 compute_ms_basis!(cache, nc, q, p, D₁) # Smooth Coefficient
 compute_ms_basis!(cache, nc, q, p, D₂) # Oscillatory Coefficient
@@ -97,7 +85,7 @@ sKms, sFms = multiscale
 # Compute the full stiffness matrix on the fine scale
 sKe_ϵ = zeros(Float64, q+1, q+1, nf)
 sFe_ϵ = zeros(Float64, q+1, nf)
-fillsKe!(sKe_ϵ, basis_cache(q), nds_fine, elem_fine, q, quad, D₁) 
+fillsKe!(sKe_ϵ, basis_cache(q), nds_fine, elem_fine, q, quad, D₃) 
 #=
 fillsKe!(sKe_ϵ, basis_cache(q), nds_fine, elem_fine, q, quad, D₁) # Smooth Coefficient
 fillsKe!(sKe_ϵ, basis_cache(q), nds_fine, elem_fine, q, quad, D₂) # Oscillatory Coefficient
@@ -129,10 +117,18 @@ end
 
 plt = plot(nds_fine, uhsol, label="Approximate solution")
 plot!(plt, nds_fine, solϵ, label="Exact solution")
+plt3 = plot()
+plot!(plt3, nds_fine, D₃.(nds_fine), lw=2, label="Diffusion Coefficient", lc=:black)
+plot!(plt3,label="Diffusion coefficient")
+#=
+plot!(plt3, nds_fine, D₁.(nds_fine), lc=:black, label="Diffusion coefficient")
+plot!(plt3, nds_fine, D₂.(nds_fine), lc=:black, label="Diffusion coefficient")
+plot!(plt3, nds_fine, D₃.(nds_fine), lc=:black, label="Diffusion coefficient")
+=#
+ylims!(plt3,(0,10))
 plt2 = plot(plt,plt3,layout=(2,1))
 
 ## Compute the errors
-# usol = u.(nds_fine)
 l2error = 0.0
 energy_error = 0.0
 bc = basis_cache(q)
@@ -143,11 +139,11 @@ for j=1:nf, jj=1:lastindex(qs)
   global l2error += ws[jj]*(dot(solϵ[elem_fine[j,:]],bc[3]) - dot(uhsol[elem_fine[j,:]],bc[3]))^2*(0.5*nf^-1)
   ∇ϕᵢ!(bc,qs[jj])
   # Smooth Coefficient
-  global energy_error += ws[jj]*D₁(x̂)*(dot(solϵ[elem_fine[j,:]],bc[3])*(2*nf) - dot(uhsol[elem_fine[j,:]],bc[3])*(2*nf))^2*(0.5*nf^-1)
+  # global energy_error += ws[jj]*D₁(x̂)*(dot(solϵ[elem_fine[j,:]],bc[3])*(2*nf) - dot(uhsol[elem_fine[j,:]],bc[3])*(2*nf))^2*(0.5*nf^-1)
   # Oscillatory coefficient
   # global energy_error += ws[jj]*D₂(x̂)*(dot(solϵ[elem_fine[j,:]],bc[3])*(2*nf) - dot(uhsol[elem_fine[j,:]],bc[3])*(2*nf))^2*(0.5*nf^-1)
   # Random coefficient
-  #global energy_error += ws[jj]*D₃(x̂)*(dot(solϵ[elem_fine[j,:]],bc[3])*(2*nf) - dot(uhsol[elem_fine[j,:]],bc[3])*(2*nf))^2*(0.5*nf^-1)
+  global energy_error += ws[jj]*D₃(x̂)*(dot(solϵ[elem_fine[j,:]],bc[3])*(2*nf) - dot(uhsol[elem_fine[j,:]],bc[3])*(2*nf))^2*(0.5*nf^-1)
 end
 
 @show l2error, energy_error
