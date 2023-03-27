65150f1765303a1b472db536261e3270d63e5c06
Balaje K <balaje6@gmail.com>
2023-03-27 14:02:06 +0200

    Fix the basis function script

--- HigherOrderMS/multiscale_basis-functions.jl
+++ HigherOrderMS/multiscale_basis-functions.jl
@@ -20,8 +20,7 @@ function compute_ms_basis(fspace::FineScaleSpace, D::Function, p::Int64, nc::Int
     last = min(nc,t+l)
     gn = start*(p+1)-p:last*(p+1)    
     stima_el = K[freenodes,freenodes]
-    lmat_el = L[:,gn]
-    lmat_el = L[fn,gn]
+    lmat_el = L[freenodes,gn]
     loadvec_el = (f1 - K[:,bnodes]*bvals)
     for _=1:p+1
       fvecs_el = [loadvec_el[freenodes]; Λ[gn, index]]
@@ -33,28 +32,4 @@ function compute_ms_basis(fspace::FineScaleSpace, D::Function, p::Int64, nc::Int
     end
   end
   basis_vec_ms
-end
-
-function compute_ms_basis_boundary_correction(fspace::FineScaleSpace, D::Function, p::Int64, nc::Int64, l::Int64,
-  patch_indices_to_global_indices::Vector{AbstractVector{Float64}}, bnodes::Vector{Int64})
-  nf = fspace.nf
-  q = fspace.q
-  basis_vec_corr = spzeros(Float64, q*nf+1, (p+1)*nc)
-  K, L, Λ = get_saddle_point_problem(fspace, D, p, nc)
-  f1 = zeros(Float64, size(K,1))
-  index = 1
-  for t=1:nc
-    tn = patch_indices_to_global_indices[t]
-    bn = [tn[1],tn[end]]
-    fn = setdiff(tn,bn)
-    start = max(1,t-l)
-    last = min(nc,t+l)
-    gn = start*(p+1)-p:last*(p+1) 
-    stima_el = K[fn,fn]
-    lmat_el = L[:,gn]
-    lmat_el_t = L[]
-    for _=1:p+1
-      lhs = [stima_el lmat_el; (lmat_el)' spzeros(Float64, length(gn), length(gn))]      
-    end
-  end
 end
