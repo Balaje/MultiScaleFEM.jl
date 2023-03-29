7e391d5b44518fd14e352f076ca63666c29ef2c8
Balaje K <balaje6@gmail.com>
2023-03-29 14:34:37 +0200

    Add the new Dirichlet boundary condition

--- HigherOrderMS/1d_Poisson_eq.jl
+++ HigherOrderMS/1d_Poisson_eq.jl
@@ -3,14 +3,16 @@ include("HigherOrderMS.jl");
 domain = (0.0,1.0)
 
 D(x) = 1.0
-f(x) = π^2*cos(π*x[1])
+f(x) = π^2*sin(π*x[1])
 
-nc = 2^5
-nf = 2^16
+nc = 2^4
+nf = 2^15
 q = 1
 p = 1
 l = 4
 qorder = 2
+
+nds_fine = LinRange(domain[1], domain[2], q*nf+1)
 
 # Get the Gridap fine-scale description
 fine_scale_space = FineScaleSpace(domain, q, qorder, nf)
@@ -19,24 +21,56 @@ fine_scale_space = FineScaleSpace(domain, q, qorder, nf)
 patch_indices_to_global_indices, coarse_indices_to_fine_indices, ms_elem = coarse_space_to_fine_space(nc, nf, l, (q,p));
 # Compute Multiscale bases
 basis_vec_ms = compute_ms_basis(fine_scale_space, D, p, nc, l, patch_indices_to_global_indices);
-# Solve the problem
+# Construct the full stiffness and load vectors
 stima = assemble_stiffness_matrix(fine_scale_space, D)
 loadvec = assemble_load_vector(fine_scale_space, f)
+
+# Solve the problem after constructing the stiffness and load vectors
 Kₘₛ = basis_vec_ms'*stima*basis_vec_ms;
 Fₘₛ = basis_vec_ms'*loadvec;
 sol = Kₘₛ\Fₘₛ
-
 # Plot
-nds_fine = LinRange(domain[1], domain[2], q*nf+1)
 sol_fine_scale = basis_vec_ms*sol
 plt3 = plot(nds_fine, sol_fine_scale, label="Homogeneous DBC")
 
-# Non-homogeneous boundary condition [1.0,1.0]
+# -- Example with non-homogenous Dirichlet boundary conditions
+f(x) = π^2*cos(π*x[1])
+ug(x) = cos(π*x[1])
+# First method for Non-homogeneous boundary condition
+loadvec = assemble_load_vector(fine_scale_space, f)
 fullnodes = 1:q*nf+1;
 bnodes = [1,q*nf+1];
-bvals = [1.0,-1.0];
+bvals = ug.(nds_fine[bnodes]);
 freenodes = setdiff(fullnodes, bnodes);
-# "The boundary correction term" - needs to be computed once
+Kₘₛ = basis_vec_ms'*stima*basis_vec_ms;
+Fₘₛ = basis_vec_ms'*loadvec;
+sol = Kₘₛ\Fₘₛ;
+# "The boundary correction term". Needs to be computed once by inverting the full stiffness matrix
 boundary_correction = (stima[freenodes,freenodes]\collect(stima[freenodes, bnodes]));
 sol_fine_scale_dbc = basis_vec_ms[freenodes,:]*sol - boundary_correction*bvals;
+# Plot
 plt4 = plot(nds_fine[freenodes], sol_fine_scale_dbc, label="Non-homogeneous DBC");
+
+#= 
+However inverting the whole stiffness matrix in Line 40 may not always be feasible.
+The remedy is to incorporate the BC using only the patch problems on the boundary 
+(See [Henning, Målqvist 2018](https://epubs.siam.org/doi/10.1137/130933198))
+=#
+# Second Method for non-homogeneous boundary condition
+fullnodes = 1:q*nf+1;
+bnodes = [1,q*nf+1];
+bvals = bvals = ug.(nds_fine[bnodes]) .+ 1.0;
+freenodes = setdiff(fullnodes, bnodes);
+# Obtain the matrix which acts as the corrector. 
+# This involves inverting 2 small matrices on the patch of the boundary elements
+Pₕug = compute_boundary_correction_matrix(fine_scale_space, D, p, nc, l, patch_indices_to_global_indices);
+# Obtain the boundary contribution by multiplying the matrix with the Dirichlet boundary
+boundary_contrib = apply_boundary_correction(Pₕug, bnodes, bvals, patch_indices_to_global_indices, p, nc, l, fine_scale_space);
+# Solve the interior problem
+Kₘₛ = basis_vec_ms'*stima[:,freenodes]*basis_vec_ms[freenodes,:];
+Fₘₛ = basis_vec_ms'*loadvec - basis_vec_ms'*(stima[:,bnodes]*bvals);
+sol2 = Kₘₛ\Fₘₛ;
+# Apply the boundary correction
+sol_fine_scale_dbc_2 = basis_vec_ms*sol2 + boundary_contrib
+# Plot
+plt5 = plot(nds_fine, sol_fine_scale_dbc_2, label="Non-homogeneous DBC");
--- HigherOrderMS/multiscale_basis-functions.jl
+++ HigherOrderMS/multiscale_basis-functions.jl
@@ -33,3 +33,59 @@ function compute_ms_basis(fspace::FineScaleSpace, D::Function, p::Int64, nc::Int
   end
   basis_vec_ms
 end
+
+"""
+For applying non homogeneous boundary condition
+(Need to check if it can be simplified further...)
+"""
+function compute_boundary_correction_matrix(fspace::FineScaleSpace, D::Function, p::Int64, nc::Int64, l::Int64,
+  patch_indices_to_global_indices::Vector{AbstractVector{Int64}})
+  # Compute the projection (solve the saddle point problems) only on the boundary elements
+  boundary_elems = [1,nc]
+  n_boundary_elems = 1:length(boundary_elems)
+  # Begin solving the problem
+  nf = fspace.nf
+  q = fspace.q
+  dims = ((l+1)*(p+1)+2) # ( ((l+1)*(p+1) = No of patch elements) + (2 = Boundary contribution of stima))
+  boundary_correction = spzeros(Float64, q*nf+1, dims*length(boundary_elems)) # 2 patch elements
+  K, L, _ = get_saddle_point_problem(fspace, D, p, nc)
+  # Begin solving 
+  for (t,i) in zip(boundary_elems,n_boundary_elems)
+    tn = patch_indices_to_global_indices[t]
+    bn = [tn[1],tn[end]]
+    fn = setdiff(tn, bn)    
+    start = max(1,t-l)
+    last = min(nc,t+l)
+    gn = start*(p+1)-p:last*(p+1)    
+    stima_el = K[fn, fn]
+    lmat_el = L[fn, gn] 
+    lhs = -[stima_el lmat_el; lmat_el' spzeros(Float64, length(gn), length(gn))]
+    # Boundary contributions of the LHS
+    rhs = collect([K[fn,bn] L[fn,gn]; (L[bn,gn])' spzeros(Float64, length(gn), length(gn))])
+    # Invert to compute the projection matrix
+    boundary_correction[fn,(dims)*i-(dims-1):dims*i] = (lhs\rhs)[1:length(fn),:] 
+  end
+  boundary_correction
+end
+function apply_boundary_correction(BC::SparseMatrixCSC{Float64,Int64}, bnodes::Vector{Int64}, bvals::Vector{Float64}, 
+  patch_indices_to_global_indices::Vector{AbstractVector{Int64}}, p::Int64, nc::Int64, l::Int64, fspace::FineScaleSpace)
+  nf = fspace.nf
+  q = fspace.q
+  boundary_elems = [1,nc]
+  n_boundary_elems = 1:length(boundary_elems)
+  boundary_correction = zeros(Float64, q*nf+1) # Zero vector to store the result  
+  _bv(bvals, i) = (i==1) ? [bvals[1], 0.0] : [0.0, bvals[2]]
+  dims = ((l+1)*(p+1)+2) # ( ((l+1)*(p+1) = No of patch elements) + (2 = Boundary contribution of stima))
+  bvec_el = zeros(Float64, dims)
+  # Compute the boundary correction
+  for (t,i) in zip(boundary_elems,n_boundary_elems)
+    tn = patch_indices_to_global_indices[t]
+    bn = [tn[1], tn[end]]
+    fn = setdiff(tn,bn)
+    bv = _bv(bvals, i)
+    bvec_el[n_boundary_elems] = bv
+    boundary_correction[fn] += BC[fn,(dims)*i-(dims-1):dims*i]*bvec_el # Compute the projection of the ith DBC value
+  end
+  boundary_correction[bnodes] = bvals # Fill in the DBC values
+  boundary_correction
+end
