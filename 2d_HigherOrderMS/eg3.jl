ad6a59e731f4c299b1a0db06213b515be34d0b22
Balaje K <balaje6@gmail.com>
2023-04-18 11:40:39 +0200

    Working patch domain extraction for triangles

--- 2d_HigherOrderMS/eg3.jl
+++ 2d_HigherOrderMS/eg3.jl
@@ -6,27 +6,25 @@ include("2d_HigherOrderMS.jl")
 domain = (0.0, 1.0, 0.0, 1.0)
 
 # Fine scale space description
-nf = 2^3
+nf = 2^7
 q = 1
-nc = 2^1
+nc = 2^4
 p = 1
 ms_space = MultiScaleFESpace(domain, q, p, nf, nc)
 
 num_coarse_cells = num_cells(get_triangulation(ms_space.UH))
 num_fine_cells = num_cells(get_triangulation(ms_space.Uh))
-σ = get_cell_node_ids(get_triangulation(ms_space.Uh))
 node_coordinates = get_node_coordinates(get_triangulation(ms_space.Uh))
 
 coarse_to_fine_elems = get_coarse_to_fine_map(num_coarse_cells, num_fine_cells)
 
 l = 1; # Patch size parameter
 
-# Get the element indices 
-patch_fine_elems = get_patch_fine_elems(ms_space, l, num_coarse_cells, coarse_to_fine_elems)
+patch_coarse_models = get_patch_triangulation(ms_space, l, num_coarse_cells)
+patch_fine_models = get_patch_triangulation(ms_space, l, num_coarse_cells, coarse_to_fine_elems)
 
-patch_fine_node_ids = lazy_map(Broadcasting(Reindex(σ)), patch_fine_elems)
-patch_cell_coordinates = get_patch_cell_coordinates(node_coordinates, patch_fine_node_ids)
-patch_node_coordinates = lazy_map(get_patch_node_coordinates, patch_cell_coordinates)
-patch_cell_types = get_patch_cell_type(get_cell_type(get_triangulation(ms_space.Uh)), patch_fine_elems)
 
-patch_grids = lazy_map()
+function save_models(patch_coarse_models, patch_fine_models, i)
+  writevtk(patch_coarse_models[i], "2d_HigherOrderMS\\"*string(i)*"-th_coarse_model")
+  writevtk(patch_fine_models[i], "2d_HigherOrderMS\\"*string(i)*"-th_fine_model")
+end
--- 2d_HigherOrderMS/multiscale_basis-functions.jl
+++ 2d_HigherOrderMS/multiscale_basis-functions.jl
@@ -56,15 +56,14 @@ function get_patch_fine_elems(ms_space::MultiScaleFESpace, l::Int64, num_coarse_
   sort.(Y)
 end
 
-function get_patch_cell_coordinates(node_coordinates, patch_fine_node_ids)
-  b = Broadcasting(Reindex(node_coordinates))
-  lazy_map(Broadcasting(b), patch_fine_node_ids)
+function get_patch_global_node_ids(patch_fine_elems, σ)
+  collect(lazy_map(Broadcasting(Reindex(σ)), patch_fine_elems))
 end
 
 function get_patch_local_cell_ids(patch_fine_elems, σ)
-  patch_fine_node_ids = collect(lazy_map(Broadcasting(Reindex(σ)), patch_fine_elems))
+  patch_fine_node_ids = get_patch_global_node_ids(patch_fine_elems, σ)
   R = sort(unique(mapreduce(permutedims, vcat, patch_fine_node_ids)))
-  for t = 1:lastindex(patch_find_node_ids)
+  for t = 1:lastindex(patch_fine_node_ids)
     for tt = 1:lastindex(R), ttt = 1:lastindex(patch_fine_node_ids[t])
       if(patch_fine_node_ids[t][ttt] == R[tt])
         patch_fine_node_ids[t][ttt] = tt  
@@ -74,10 +73,48 @@ function get_patch_local_cell_ids(patch_fine_elems, σ)
   patch_fine_node_ids
 end
 
-function get_patch_node_coordinates(cell_coordinates)
-  unique(reduce(vcat, cell_coordinates))
+function get_patch_node_coordinates(node_coordinates, patch_fine_node_ids)
+  R = sort(unique(mapreduce(permutedims, vcat, patch_fine_node_ids)))
+  node_coordinates[R]
 end
 
 function get_patch_cell_type(cell_types, patch_elem_indices)
   lazy_map(Broadcasting(Reindex(cell_types)), patch_elem_indices)
+end
+
+function get_patch_triangulation(ms_space::MultiScaleFESpace, l::Int64, num_coarse_cells::Int64, coarse_to_fine_elems)
+  node_coordinates = get_node_coordinates(get_triangulation(ms_space.Uh))
+  σ = get_cell_node_ids(get_triangulation(ms_space.Uh))
+  cell_types = get_cell_type(get_triangulation(ms_space.Uh))
+
+  patch_fine_elems = get_patch_fine_elems(ms_space, l, num_coarse_cells, coarse_to_fine_elems) # Get the indices of the fine-scale elements inside the patch
+  patch_fine_node_ids = get_patch_global_node_ids(patch_fine_elems, σ) # Get the global numbering of the nodes inside the patch to get the coordinates
+  patch_fine_local_node_ids = Table.(lazy_map(get_patch_local_cell_ids, patch_fine_elems, Gridap.Arrays.Fill(σ,num_coarse_cells))) # Get the local numbering of the nodes inside the patch to build the triangulation
+  patch_node_coordinates = lazy_map(get_patch_node_coordinates, Gridap.Arrays.Fill(node_coordinates, num_coarse_cells), patch_fine_node_ids) # Get the node coordinates in the patch
+  patch_cell_types = get_patch_cell_type(cell_types, patch_fine_elems) # Get the cell types in the patch
+  
+  # Construct the grids from the node coordinates and the connectivity
+  patch_grids = lazy_map(UnstructuredGrid, patch_node_coordinates, patch_fine_local_node_ids, Gridap.Arrays.Fill(get_reffes(get_triangulation(ms_space.Uh)), num_coarse_cells), patch_cell_types)
+  patch_grids_topology = lazy_map(GridTopology, patch_grids)
+  patch_face_labelling = lazy_map(FaceLabeling, patch_grids_topology)
+  # Construct the DiscreteModels for the Triangulation
+  lazy_map(DiscreteModel, patch_grids, patch_grids_topology, patch_face_labelling)
+end
+
+function get_patch_triangulation(ms_spaces::MultiScaleFESpace, l::Int64, num_coarse_cells::Int64)
+  node_coordinates = get_node_coordinates(get_triangulation(ms_space.UH))
+  σ = get_cell_node_ids(get_triangulation(ms_space.UH))
+  cell_types = get_cell_type(get_triangulation(ms_space.UH))
+
+  patch_coarse_elems = lazy_map(get_patch_coarse_elem, Gridap.Arrays.Fill(ms_spaces, num_coarse_cells), Gridap.Arrays.Fill(l, num_coarse_cells), 1:num_coarse_cells)
+  patch_coarse_node_ids = collect(lazy_map(Broadcasting(Reindex(σ)), patch_coarse_elems))
+  patch_coarse_local_ids = Table.(lazy_map(get_patch_local_cell_ids, patch_coarse_elems, Gridap.Arrays.Fill(σ,num_coarse_cells)))
+  patch_node_coordinates = lazy_map(get_patch_node_coordinates, Gridap.Arrays.Fill(node_coordinates, num_coarse_cells), patch_coarse_node_ids)
+  patch_cell_types = get_patch_cell_type(cell_types, patch_coarse_elems)
+
+  patch_grids = lazy_map(UnstructuredGrid, patch_node_coordinates, patch_coarse_local_ids, Gridap.Arrays.Fill(get_reffes(get_triangulation(ms_space.Uh)), num_coarse_cells), patch_cell_types)
+  patch_grids_topology = lazy_map(GridTopology, patch_grids)
+  patch_face_labelling = lazy_map(FaceLabeling, patch_grids_topology)
+  # Construct the DiscreteModels for the Triangulation
+  lazy_map(DiscreteModel, patch_grids, patch_grids_topology, patch_face_labelling)
 end
